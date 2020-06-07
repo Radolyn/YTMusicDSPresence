18d6bb2d39c4d04107fe28478868b35fee2e577b
AlexeyZavar <alexeyzavar@gmail.com>
2020-06-08 02:36:52 +0300

    Add README.md and TamperMonkey script

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # YTMusicDSPresence
+
 Discord presence for YouTube Music
+
+## Installation
+
+You need to install TamperMonkey ([Chrome](https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo)\\[Opera](https://addons.opera.com/ru/extensions/details/tampermonkey-beta)\\[Firefox](https://addons.mozilla.org/ru/firefox/addon/tampermonkey/))
+
+Click to the link and install script:
+
+[youtube_music.user.js](https://raw.githubusercontent.com/Radolyn/YTMusicDSPresence/master/youtube_music.user.js)
+
+Download proxy from [Releases](https://github.com/Radolyn/YTMusicDSPresence/releases) tab
+
+Double click on it & start listening your favourite music from YouTube Music
+
+## Screenshot
+
+![Screenshot](https://radolyn.com/shared/youtube_music.png)
+
+## Configuration
+
+```conf
+applicationId=718862357282947144 # Application id
+port=1339 # Port
+timeout=5000 # Browser timeout (if we can't get data >timeout, then we're cleaning rich presence)
+largeImageKey=1200px-youtube_music_logo_svg # Big icon
+largeImageText=YouTube Music # Big icon text
+paused=pause_v2 # Paused icon
+playing=play # Playing icon
+pausedText=Простаивает # Paused text
+playingText=Слушает # Playing text
+enableJoinRequestSounds=true # Enable join request sounds
+enableAutoUrlOpen=true # Open YouTube Music on join
+enableInviteFeature=true # Enable invite feature (your friends need to download this app too)
+hideConsole=false # Hide app console
+```
+
+## Bugs
+
+- Works on English only
---
+++ youtube_music.user.js
@@ -0,0 +1,97 @@
+// ==UserScript==
+// @name         YouTubeMusic Transmitter
+// @version      0.1
+// @description  Stream your YouTube Music name & position to Discord
+// @author       Radolyn
+// @license      Apache-2.0
+// @match        https://music.youtube.com/*
+// @supportURL   https://github.com/Radolyn/YTMusicDSPresence
+// @updateURL    https://raw.githubusercontent.com/Radolyn/YTMusicDSPresence/master/youtube_music.user.js
+// @downloadURL  https://raw.githubusercontent.com/Radolyn/YTMusicDSPresence/master/youtube_music.user.js
+// @homepage     https://github.com/Radolyn/YTMusicDSPresence
+// @namespace    https://github.com/Radolyn/YTMusicDSPresence
+// @require      https://momentjs.com/downloads/moment.js
+// @run-at       document-end
+// @grant        GM_xmlhttpRequest
+// ==/UserScript==
+
+(function () {
+  "use strict";
+  function sendActivity(data2) {
+    GM_xmlhttpRequest({
+      method: "POST",
+      url: "http://localhost:1339/presence/",
+      data: JSON.stringify(data2),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  }
+  function getSong() {
+    return document.getElementsByClassName(
+      "title style-scope ytmusic-player-bar"
+    )[0].innerText;
+  }
+  function getDuration() {
+    return document
+      .getElementsByClassName("time-info style-scope ytmusic-player-bar")[0]
+      .innerText.split("/");
+  }
+  function getEnd() {
+    var array = getDuration();
+    var diff1;
+    var diff2;
+    var time = Math.round(new Date() / 1);
+    if (array[0].split(":").length === 2) {
+      diff1 = moment(array[0], "mm:ss").unix();
+    } else {
+      diff1 = moment(array[0], "HH:mm:ss").unix();
+    }
+    if (array[1].split(":").length === 2) {
+      diff2 = moment(array[1], "mm:ss").unix();
+    } else {
+      diff2 = moment(array[1], "HH:mm:ss").unix();
+    }
+    var end = diff2 * 1000 - diff1 * 1000 + time;
+    return [time, end];
+  }
+  function getArtist() {
+    return document
+      .getElementsByClassName("byline style-scope ytmusic-player-bar")[0]
+      .title.replace("•");
+  }
+  function getState() {
+    var aria = document
+      .getElementsByClassName(
+        "play-pause-button style-scope ytmusic-player-bar"
+      )[0]
+      .ariaLabel.toLowerCase();
+    return "play" === aria;
+  }
+  function getId() {
+    var link = window.location.href;
+    var sliceBefore = function (str, pattern) {
+      return str.slice(str.indexOf(pattern) + pattern.length);
+    };
+    var sliceAfter = function (str, pattern) {
+      return str.slice(0, str.indexOf(pattern));
+    };
+    return sliceAfter(sliceBefore(link, "watch?v="), "&list=");
+  }
+  setInterval(function () {
+    var song = getSong();
+    var times = getEnd();
+    var artist = getArtist();
+    var state = getState();
+    var id = getId();
+    var array = {
+      song: song,
+      id: id,
+      artist: artist,
+      start: times[0],
+      end: times[1],
+      paused: state,
+    };
+    sendActivity(array);
+  }, 1000);
+})();
