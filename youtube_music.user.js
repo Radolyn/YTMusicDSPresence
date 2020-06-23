dada3804364d8d8e7e055c4f5586cf0fe2aec07a
MaxPlays35 <35427519+MaxPlays35@users.noreply.github.com>
2020-06-23 18:59:38 +0100

    Update version script

--- youtube_music.user.js
+++ youtube_music.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         YouTubeMusic Transmitter
-// @version      0.3
+// @version      0.4
 // @description  Stream your YouTube Music name & position to Discord
 // @author       Radolyn
 // @license      Apache-2.0
